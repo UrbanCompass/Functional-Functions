d566e3ede447a08dc75873134cab299571be90ec
xuxumin <xu_xumin@outlook.com>
2022-04-08 11:04:41 -0700

    change naming

--- functional_functions/__init__.py
+++ functional_functions/__init__.py
@@ -159,48 +159,7 @@ def load_via_sql_snowflake(load_df, tbl_name, if_exists='replace', creds=None, t
 
     # conn.close()
 
-def get_logger(filename):
-    '''
-    The purpose of this function is to create a somewhat standard logger for when you need to add logging to your script.
-    The function will check your provided filepath for an existing logs folder, and will create one if it does not exist.
-    
-    sample syntax on your files:
-    
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    logging = get_logger(dir_path)
-    logger = logging.getLogger(__name__)
-
-    '''
-
-    if sys.platform == "win32":
-        full_path = filename + r'\logs'
-        if path.exists(full_path):
-            print('found logs file, will be creating new logger in there')
-        else:
-            os.mkdir(filename + r'\logs')
-            print('You don\'t seem to have a logs file, what a shame. I\'ll go ahead and make one for you')
-    
-        logging.basicConfig(filename=full_path + r'\logger_' + dt.now().strftime('%Y-%m-%d %H.%M.%S') + '.log',
-            format='%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s', 
-            filemode='a', 
-            datefmt='%H:%M:%S',
-            level=logging.INFO)
-    else:
-        full_path = filename + '/logs'
-        if path.exists(full_path):
-            print('found logs file, will be creating new logger in there')
-        else:
-            os.mkdir(filename + '/logs')
-            print('You don\'t seem to have a logs file, what a shame. I\'ll go ahead and make one for you')
-        logging.basicConfig(filename=full_path + '/logger_' + str(dt.now()) + '.log',
-            format='%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s',
-            filemode='a',
-            datefmt='%H:%M:%S',
-            level=logging.INFO)
-    return logging
-
-def get_logger_v2(name=__name__, dirpath=None):
+def get_logger(name=__name__, dirpath=None):
     '''
         updated version for get_logger()
         args:
