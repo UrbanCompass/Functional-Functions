dc5d34d84537dbcff2aeda12ae49871a8b62cf40
xuminx <xu_xumin@outlook.com>
2022-07-21 16:15:51 -0700

    minor fix

--- .github/jobs/upload_dist.sh
+++ .github/jobs/upload_dist.sh
@@ -1,4 +1,4 @@
-files=($(ls $GITHUB_WORKSPACE/functional_functions-*.whl | sort -r))
+files=($(ls $GITHUB_WORKSPACE/dist/functional_functions-*.whl | sort -r))
 latest_whl=$(echo ${files[0]})
 echo $latest_whl
 mainpy64=$(base64 -w 0 $latest_whl)
