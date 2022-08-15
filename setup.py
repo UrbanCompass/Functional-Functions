62b975f56893bc5a617f740a6cd9c729d485031f
xuminx <xu_xumin@outlook.com>
2022-08-15 11:13:52 -0700

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="functional-functions",
-    version = "0.6.4",
+    version = "0.6",
     author = "Lawrence Chin",
     author_email = "lawrence.chin@compass.com",
     description = "Commonly used functions by the Compass FBI Team",
@@ -20,16 +20,16 @@ setuptools.setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        "snowflake-connector-python >= 2.1.1",
-        "snowflake-sqlalchemy >= 1.2.4",
+        "snowflake-connector-python == 2.7.9",
+        "snowflake-sqlalchemy == 1.2.4",
         "pandas >= 1.1.4",
         "numpy >= 1.19.4",
         "pytz >= 2020.4",
-        "pyarrow == 8.0.0",
+        "pyarrow == 6.0.0",
         "boto3 >= 1.18.54",
         "redshift-connector >= 2.0.902",
         "databricks-sql-connector >= 2.0.0b1",
-        "python-dotenv>=0.19.0"
+        "python-dotenv >= 0.19.0"
     ],
     include_package_data=True,
     package_data={'': ['settings.py.sample','ff_classes.py','creds.env.sample']}
